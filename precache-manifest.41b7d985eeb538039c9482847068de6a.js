b9bb55400e21a691f46d6c89c0605ab332703ad4
rayhanuddin2019 <php.devs.ninja@gmail.com>
2019-12-21 22:36:26 +0600

    Updates

---
+++ precache-manifest.41b7d985eeb538039c9482847068de6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919fdc3847e530c66d615b163bbfa7ce",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "35ff3fa725fd8caaa656",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ad9a39b39698138b3f8",
+    "url": "/react/static/js/2.603542f0.chunk.js"
+  },
+  {
+    "revision": "9fc635aceec4ff3bb823043ab022e718",
+    "url": "/react/static/js/2.603542f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35ff3fa725fd8caaa656",
+    "url": "/react/static/js/main.c495fea4.chunk.js"
+  },
+  {
+    "revision": "77af834e0d84641c40e9",
+    "url": "/react/static/js/runtime-main.c583701e.js"
+  }
+]);
